0331c1a584f1bac6ed73c758bbed760e86bc8787
reodinas <120348461+reodinas@users.noreply.github.com>
2023-03-22 18:05:26 +0900

    수정

--- resources/order.py
+++ resources/order.py
@@ -33,11 +33,11 @@ class OrderListResource(Resource):
                     from orders
                     where userId = %s
                     order by createdAt desc;
-                    limit '''+offset+''', ''' +limit+'''
+                    limit '''+offset+''', ''' +limit+''';
                     '''
             record = (userId, )
 
-            cursor = connection.cursor(dictionary=True, buffered=True)
+            cursor = connection.cursor(dictionary=True)
             cursor.execute(query, record)
             result_list = cursor.fetchall()
 
